603c8057c6d12540e296d291edc5e460bf1a057b
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2024-11-11 19:43:25 -0500

    - fix

--- Repo/CommunityGamesHub/loading.gd
+++ Repo/CommunityGamesHub/loading.gd
@@ -1,7 +1,51 @@
 extends Control
 
+# HTTPRequest node instance
+var http: HTTPRequest
 
-# Called when the node enters the scene tree for the first time.
+# Download function
+func download(link: String) -> void:
+	http = HTTPRequest.new()
+	add_child(http)
+	# Connect the request_completed signal
+	http.request_completed.connect(_http_request_completed)
+	
+	# Make the HTTP request
+	var request = http.request(link)
+	if request != OK:
+		push_error("HTTP request error: %d" % request)
+
+# Callback for when the HTTP request is completed
+func _http_request_completed(result: int, _response_code: int, _headers: Array, _body: PackedByteArray) -> void:
+	# Check if the request was successful
+	if result != OK:
+		push_error("Download failed: %d" % result)
+		return
+
+	# Initialize XMLParser to parse the downloaded content
+	var parser = XMLParser.new()
+	parser.open_buffer(_body)
+	
+	# Store all URLs in an array
+	var urls = []
+	
+	# Parse the XML content
+	while parser.read() != ERR_FILE_EOF:
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
+			var node_name = parser.get_node_name()
+			
+			# Check if the node is a 'url' element and capture its content
+			if node_name == "url":
+				parser.read()  # Move to the text node inside <url>
+				if parser.get_node_type() == XMLParser.NODE_TEXT:
+					urls.append(parser.get_node_data())
+	
+	# Print all collected URLs
+	print("Collected URLs:", urls)
+
+	# Clean up the HTTPRequest node after use
+	http.queue_free()
+
+# Called when the node enters the scene tree
 func _ready() -> void:
-	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	pass
+	download("https://raw.githubusercontent.com/GodotCommunityGamesOrg/Community-Game-Hub/bf88573b43334226d3585b43f9f3afd797582762/Games/Log.xml")
